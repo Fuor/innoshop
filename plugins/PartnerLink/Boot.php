d4b251811e150cd95a6e333851b40e9b2d9b4efb
Fuor <fuor88@qq.com>
2025-04-29 10:55:35 +0800

    test

--- plugins/PartnerLink/Boot.php
+++ plugins/PartnerLink/Boot.php
@@ -10,55 +10,19 @@
 namespace Plugin\PartnerLink;
 
 use Plugin\PartnerLink\Models\PartnerLink;
-use Barryvdh\Debugbar\Facades\Debugbar;
 
 class Boot
 {
     public function init(): void
     {
-
-        $eventy = app('eventy');
-
-        // 直接在 filter 属性中添加回调
-        $tag = 'component.sidebar.plugin.routes';
-        $priority = 10;
-        $callback = function ($data) {
-            \Illuminate\Support\Facades\Log::info('钩子执行前', [
-                'data' => $data
-            ]);
-
-            $menuItem = [
+        listen_hook_filter('component.sidebar.plugin.routes', function ($data) {
+            $data[] = [
                 'route' => 'partner_links.index',
                 'title' => '友情链接',
-                'icon' => 'link',
-                'sort' => 100
             ];
 
-            $result = is_array($data) ? array_merge($data, [$menuItem]) : [$menuItem];
-
-            \Illuminate\Support\Facades\Log::info('钩子执行后', [
-                'result' => $result
-            ]);
-
-            return $result;
-        };
-
-        // 手动添加过滤器
-        $reflection = new \ReflectionClass($eventy);
-        $filterProp = $reflection->getProperty('filter');
-        $filterProp->setAccessible(true);
-
-        $filters = $filterProp->getValue($eventy);
-        if (!isset($filters['TorMorten\Eventy\Filter'][$tag][$priority])) {
-            $filters['TorMorten\Eventy\Filter'][$tag][$priority] = [];
-        }
-        $filters['TorMorten\Eventy\Filter'][$tag][$priority][] = $callback;
-
-        $filterProp->setValue($eventy, $filters);
-
-        \Illuminate\Support\Facades\Log::info('手动注册后的钩子状态', [
-            'filters' => $filterProp->getValue($eventy)
-        ]);
+            return $data;
+        });
 
         listen_blade_insert('layouts.footer.top', function ($data) {
             $data['links'] = PartnerLink::query()->where('active', 1)->limit(10)->get();

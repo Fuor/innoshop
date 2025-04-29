a8cc1d64455c4b51e3f9ff3e5e4aa45c0a6ded82
Fuor <fuor88@qq.com>
2025-04-29 13:55:32 +0800

    test

--- plugins/PartnerLink/Views/panel/index.blade.php
+++ plugins/PartnerLink/Views/panel/index.blade.php
@@ -44,7 +44,7 @@
         @endforeach
       </tbody>
       @else
-      <tbody><tr><td colspan="5"><x-panel-no-data /></td></tr></tbody>
+      <tbody><tr><td colspan="5"><x-common-no-data :width="240 /></td></tr></tbody>
       @endif
     </table>
     {{ $items->withQueryString()->links('panel::vendor/pagination/bootstrap-4') }}
